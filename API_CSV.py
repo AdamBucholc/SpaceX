53179de9bc66ed6fc19255a6b870c33198285927
AdamBucholc <83245055+AdamBucholc@users.noreply.github.com>
2021-04-26 22:48:28 +0200

    Update API_CSV.py

--- API_CSV.py
+++ API_CSV.py
@@ -7,29 +7,29 @@ url = "https://api.spacexdata.com/v3/launches"
 
 response = requests.get(url)  # Data download.
 
-csv_w=csv.writer(open("flights.csv", "w"), lineterminator="\n") # Create 'csv_w' object and open file 'flights.csv'.
+with open("flights.csv", mode='w') as flights_file:
+    csv_w = csv.writer(flights_file)
 
-csv_w.writerow([        #Save strings in a CSV file.
+    csv_w.writerow([         #Save strings in a CSV file.
     "flight_number:", 
     "mission_name:",
     "rocket_id:",
     "rocket_number:", 
     "launch_date_utc:", 
     "video_link: "
-])
+    ])
 
 
-for informations in response.json():  #Loop that saves data in a CSV file.
+    for informations in response.json():  #Loop that saves data in a CSV file.
     
-    csv_w.writerow([
-        informations["flight_number"],
-        informations["mission_name"], 
-        informations["rocket"]["rocket_id"],
-        informations["rocket"]['rocket_name'],
-        informations["launch_date_utc"],
-        informations["links"]["video_link"]
-        ])
-
+        csv_w.writerow([
+            informations["flight_number"],
+            informations["mission_name"], 
+            informations["rocket"]["rocket_id"],
+            informations["rocket"]['rocket_name'],
+            informations["launch_date_utc"],
+            informations["links"]["video_link"]
+            ])
 
 
 
